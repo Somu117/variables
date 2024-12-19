66baa0a31af5e1661b78147b023bf550bd7c93d4
Somu117 <someswararaobotta@gmail.com>
2024-12-19 21:04:21 +0530

    variables

---
+++ task1.js
@@ -0,0 +1,21 @@
+
+    let somesh=prompt("enter user name")
+    let ibm = prompt("enter money")
+    let webdeveloper = prompt("enter role")
+    let date = prompt("enter date")
+    let money = prompt("enter money")
+
+        let pro = {
+       
+            student: somu,
+            company: ibm,
+            package: money,
+            
+            role : webdeveloper,
+            joindate: date 
+
+
+        }
+   
+// mahesh got placed in  sticky-soft company as a developer with package of joining is on 01/01/2025
+alert(`${pro.student} got placed in ${pro.company} company as a ${pro.role} with package of ${pro.package} and joining on ${pro.joindate} `)
